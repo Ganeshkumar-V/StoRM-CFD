3e058a8edd834fbb7d5b819302c602ab850b38b3
Ganeshkumar-V <ganeshkv@iitgn.ac.in>
2023-05-22 19:10:41 +0530

    addition of drag model to RAS

--- library/multiphaseCompressibleTurbulenceModels/multiphaseKineticTheoryModel/multiphaseKineticTheoryModel.C
+++ library/multiphaseCompressibleTurbulenceModels/multiphaseKineticTheoryModel/multiphaseKineticTheoryModel.C
@@ -387,99 +387,99 @@ void Foam::RASModels::multiphaseKineticTheoryModel::correct()
     // Calculating the radial distribution function
     gs0_ = radialModel_->g0(alpha, alphaMinFriction_, alphaMax_);
 
-    // if (!equilibrium_)
-    // {
-    //     const surfaceScalarField& alphaRhoPhi = alphaRhoPhi_;
-    //     const volVectorField& Uc_ = otherPhase_.U();
-    //     const volVectorField& U = U_;
-    //     // refCast<const twoPhaseSystem>(phase_.fluid()).otherPhase(phase_).U();
-    //
-    //     // Particle viscosity (Table 3.2, p.47)
-    //     nut_ = viscosityModel_->nu(alpha, Theta_, gs0_, rho, da, e_);
-    //
-    //     volScalarField ThetaSqrt("sqrtTheta", sqrt(Theta_));
-    //
-    //     // Bulk viscosity  p. 45 (Lun et al. 1984).
-    //     lambda_ = (4.0/3.0)*sqr(alpha)*da*gs0_*(1 + e_)*ThetaSqrt/sqrtPi;
-    //
-    //     // Stress tensor, Definitions, Table 3.1, p. 43
-    //     volSymmTensorField tau
-    //     (
-    //         rho*(2*nut_*D + (lambda_ - (2.0/3.0)*nut_)*tr(D)*I)
-    //     );
-    //
-    //     // Dissipation (Eq. 3.24, p.50)
-    //     volScalarField gammaCoeff
-    //     (
-    //         "gammaCoeff",
-    //         12*(1 - sqr(e_))
-    //        *max(sqr(alpha), residualAlpha_)
-    //        *rho*gs0_*(1.0/da)*ThetaSqrt/sqrtPi
-    //     );
-    //
-    //     // Drag
-    //     volScalarField beta
-    //     (
-    //         refCast<const twoPhaseSystem>(phase_.fluid()).Kd()
-    //     );
-    //
-    //     // Eq. 3.25, p. 50 Js = J1 - J2
-    //     volScalarField J1("J1", 3*beta);
-    //     volScalarField J2
-    //     (
-    //         "J2",
-    //         0.25*sqr(beta)*da*magSqr(U - Uc_)
-    //        /(
-    //            max(alpha, residualAlpha_)*rho
-    //           *sqrtPi*(ThetaSqrt + ThetaSmallSqrt)
-    //         )
-    //     );
-    //
-    //     // particle pressure - coefficient in front of Theta (Eq. 3.22, p. 45)
-    //     volScalarField PsCoeff
-    //     (
-    //         granularPressureModel_->granularPressureCoeff
-    //         (
-    //             alpha,
-    //             gs0_,
-    //             rho,
-    //             e_
-    //         )
-    //     );
-    //
-    //     // 'thermal' conductivity (Table 3.3, p. 49)
-    //     kappa_ = conductivityModel_->kappa(alpha, Theta_, gs0_, rho, da, e_);
-    //
-    //     fv::options& fvOptions(fv::options::New(mesh_));
-    //
-    //     // Construct the granular temperature equation (Eq. 3.20, p. 44)
-    //     // NB. note that there are two typos in Eq. 3.20:
-    //     //     Ps should be without grad
-    //     //     the laplacian has the wrong sign
-    //     fvScalarMatrix ThetaEqn
-    //     (
-    //         1.5*
-    //         (
-    //             fvm::ddt(alpha, rho, Theta_)
-    //           + fvm::div(alphaRhoPhi, Theta_)
-    //           - fvc::Sp(fvc::ddt(alpha, rho) + fvc::div(alphaRhoPhi), Theta_)
-    //         )
-    //       - fvm::laplacian(kappa_, Theta_, "laplacian(kappa,Theta)")
-    //      ==
-    //       - fvm::SuSp((PsCoeff*I) && gradU, Theta_)
-    //       + (tau && gradU)
-    //       + fvm::Sp(-gammaCoeff, Theta_)
-    //       + fvm::Sp(-J1, Theta_)
-    //       + fvm::Sp(J2/(Theta_ + ThetaSmall), Theta_)
-    //       + fvOptions(alpha, rho, Theta_)
-    //     );
-    //
-    //     ThetaEqn.relax();
-    //     fvOptions.constrain(ThetaEqn);
-    //     ThetaEqn.solve();
-    //     fvOptions.correct(Theta_);
-    // }
-    // else
+    if (!equilibrium_)
+    {
+        const surfaceScalarField& alphaRhoPhi = alphaRhoPhi_;
+        const volVectorField& Uc_ = otherPhase_.U();
+        const volVectorField& U = U_;
+        // refCast<const twoPhaseSystem>(phase_.fluid()).otherPhase(phase_).U();
+
+        // Particle viscosity (Table 3.2, p.47)
+        nut_ = viscosityModel_->nu(alpha, Theta_, gs0_, rho, da, e_);
+
+        volScalarField ThetaSqrt("sqrtTheta", sqrt(Theta_));
+
+        // Bulk viscosity  p. 45 (Lun et al. 1984).
+        lambda_ = (4.0/3.0)*sqr(alpha)*da*gs0_*(1 + e_)*ThetaSqrt/sqrtPi;
+
+        // Stress tensor, Definitions, Table 3.1, p. 43
+        volSymmTensorField tau
+        (
+            rho*(2*nut_*D + (lambda_ - (2.0/3.0)*nut_)*tr(D)*I)
+        );
+
+        // Dissipation (Eq. 3.24, p.50)
+        volScalarField gammaCoeff
+        (
+            "gammaCoeff",
+            12*(1 - sqr(e_))
+           *max(sqr(alpha), residualAlpha_)
+           *rho*gs0_*(1.0/da)*ThetaSqrt/sqrtPi
+        );
+
+        // Drag
+        volScalarField beta
+        (
+            this->db().lookupObject<volScalarField>("Kd.particlesInGas")
+        );
+
+        // Eq. 3.25, p. 50 Js = J1 - J2
+        volScalarField J1("J1", 3*beta);
+        volScalarField J2
+        (
+            "J2",
+            0.25*sqr(beta)*da*magSqr(U - Uc_)
+           /(
+               max(alpha, residualAlpha_)*rho
+              *sqrtPi*(ThetaSqrt + ThetaSmallSqrt)
+            )
+        );
+
+        // particle pressure - coefficient in front of Theta (Eq. 3.22, p. 45)
+        volScalarField PsCoeff
+        (
+            granularPressureModel_->granularPressureCoeff
+            (
+                alpha,
+                gs0_,
+                rho,
+                e_
+            )
+        );
+
+        // 'thermal' conductivity (Table 3.3, p. 49)
+        kappa_ = conductivityModel_->kappa(alpha, Theta_, gs0_, rho, da, e_);
+
+        fv::options& fvOptions(fv::options::New(mesh_));
+
+        // Construct the granular temperature equation (Eq. 3.20, p. 44)
+        // NB. note that there are two typos in Eq. 3.20:
+        //     Ps should be without grad
+        //     the laplacian has the wrong sign
+        fvScalarMatrix ThetaEqn
+        (
+            1.5*
+            (
+                fvm::ddt(alpha, rho, Theta_)
+              + fvm::div(alphaRhoPhi, Theta_)
+              - fvc::Sp(fvc::ddt(alpha, rho) + fvc::div(alphaRhoPhi), Theta_)
+            )
+          - fvm::laplacian(kappa_, Theta_, "laplacian(kappa,Theta)")
+         ==
+          - fvm::SuSp((PsCoeff*I) && gradU, Theta_)
+          + (tau && gradU)
+          + fvm::Sp(-gammaCoeff, Theta_)
+          + fvm::Sp(-J1, Theta_)
+          + fvm::Sp(J2/(Theta_ + ThetaSmall), Theta_)
+          + fvOptions(alpha, rho, Theta_)
+        );
+
+        ThetaEqn.relax();
+        fvOptions.constrain(ThetaEqn);
+        ThetaEqn.solve();
+        fvOptions.correct(Theta_);
+    }
+    else
     {
         // Equilibrium => dissipation == production
         // Eq. 4.14, p.82
